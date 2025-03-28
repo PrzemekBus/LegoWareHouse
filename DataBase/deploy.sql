816a16cbd494fce75ae763a34f1e56652e702036
pbus <gramp.fot@gmail.com>
2025-03-28 18:10:28 +0100

    Auto-generated deploy.sql with updated file references

--- DataBase/deploy.sql
+++ DataBase/deploy.sql
@@ -1,20 +1,25 @@
--------------Initialization tables-------------
+-------------Initialization script-------------
 
--------------Initialization tables-------------
+-------------constraints-------------
+@constraints/lwh_c_dictionaries.sql
+@constraints/lwh_c_product_images.sql
+@constraints/lwh_c_products.sql
+@constraints/lwh_c_purchase_history.sql
+@constraints/lwh_c_sales_history.sql
+@constraints/lwh_c_stock.sql
+@constraints/lwh_c_stock_products.sql
+
+-------------scripts-------------
+@scripts/init_dictionaries.sql
+
+-------------tables-------------
 @tables/lwh_dictionaries.sql
+@tables/lwh_platforms.sql
 @tables/lwh_product_images.sql
 @tables/lwh_products.sql
+@tables/lwh_purchase_history.sql
+@tables/lwh_sales_history.sql
 @tables/lwh_stock.sql
 @tables/lwh_stock_products.sql
 @tables/lwh_theme.sql
 @tables/lwh_users.sql
-
--------------Initialization constraints-------------
-@constraints/lwh_c_dictionaries.sql
-@constraints/lwh_c_product_images.sql
-@constraints/lwh_c_products.sql
-@constraints/lwh_c_stock.sql
-@constraints/lwh_c_stock_products.sql
-
--------------Initialization scripts-------------
-@scripts/init_dictionaries.sql
