38cba75d1949c8b264970b743945890662d1d490
pbus <gramp.fot@gmail.com>
2025-03-28 14:20:18 +0100

    Auto-generated deploy.sql with updated file references

--- DataBase/deploy.sql
+++ DataBase/deploy.sql
@@ -1,9 +1,6 @@
 -------------Initialization tables-------------
 @tables/lwh_dictionaries.sql
-@tables/lwh_listings.sql
 @tables/lwh_products.sql
-@tables/lwh_purchases.sql
-@tables/lwh_sales.sql
 @tables/lwh_stock.sql
 @tables/lwh_stock_products.sql
 @tables/lwh_users.sql
@@ -12,14 +9,11 @@
 @scripts/init_dictionaries.sql
 
 -------------Initialization indexes-------------
-@indexes/lwh_inedexes.sql
+@indexes/*.sql
 
 -------------Initialization constraints-------------
 @constraints/lwh_c_dictionaries.sql
-@constraints/lwh_c_listings.sql
 @constraints/lwh_c_products.sql
-@constraints/lwh_c_purchases.sql
-@constraints/lwh_c_sales.sql
 @constraints/lwh_c_stock.sql
 @constraints/lwh_c_stock_products.sql
 
