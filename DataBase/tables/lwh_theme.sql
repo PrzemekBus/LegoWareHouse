3a5ef1a35a7e791195296d4760820297e4f447b2
pbus <gramp.fot@gmail.com>
2025-03-29 12:03:38 +0100

    modify theme table name

--- DataBase/tables/lwh_theme.sql
+++ DataBase/tables/lwh_theme.sql
@@ -1,4 +1,4 @@
-create table th_theme (
+create table lwh_theme (
     th_id number generated always as identity primary key,
     th_name varchar2(100) not null unique,
     th_description varchar2(500),
@@ -11,14 +11,14 @@ create table th_theme (
     th_modified_by number
 );
 
-comment on table th_theme is 'Table storing LEGO set themes (series)';
-comment on column th_theme.th_id is 'Unique identifier for the theme';
-comment on column th_theme.th_name is 'Name of the LEGO theme (e.g., Star Wars, Technic)';
-comment on column th_theme.th_description is 'Description of the LEGO theme';
-comment on column th_theme.th_logo_html is 'HTML representation of the theme logo';
-comment on column th_theme.th_logo_blob is 'Binary representation of the theme logo';
-comment on column th_theme.th_is_active is 'Indicates if the theme is active (1 = yes, 0 = no)';
-comment on column th_theme.th_created_at is 'Timestamp when the theme was created';
-comment on column th_theme.th_created_by is 'User who created the theme';
-comment on column th_theme.th_modified_at is 'Timestamp when the theme was last modified';
-comment on column th_theme.th_modified_by is 'User who last modified the theme';
+comment on table lwh_theme is 'Table storing LEGO set themes (series)';
+comment on column lwh_theme.th_id is 'Unique identifier for the theme';
+comment on column lwh_theme.th_name is 'Name of the LEGO theme (e.g., Star Wars, Technic)';
+comment on column lwh_theme.th_description is 'Description of the LEGO theme';
+comment on column lwh_theme.th_logo_html is 'HTML representation of the theme logo';
+comment on column lwh_theme.th_logo_blob is 'Binary representation of the theme logo';
+comment on column lwh_theme.th_is_active is 'Indicates if the theme is active (1 = yes, 0 = no)';
+comment on column lwh_theme.th_created_at is 'Timestamp when the theme was created';
+comment on column lwh_theme.th_created_by is 'User who created the theme';
+comment on column lwh_theme.th_modified_at is 'Timestamp when the theme was last modified';
+comment on column lwh_theme.th_modified_by is 'User who last modified the theme';
