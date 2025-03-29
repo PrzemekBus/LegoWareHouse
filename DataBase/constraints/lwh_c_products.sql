924c9892a3c09c1f3cf4771c1f11b3c66a6a9606
pbus <gramp.fot@gmail.com>
2025-03-29 12:03:59 +0100

    fix tables names in constraint

--- DataBase/constraints/lwh_c_products.sql
+++ DataBase/constraints/lwh_c_products.sql
@@ -1,3 +1,3 @@
-alter table lwh_products add constraint fk_product_created_by foreign key (prod_created_by) references users(usr_id);
-alter table lwh_products add constraint fk_product_modified_by foreign key (prod_modified_by) references users(usr_id);
-alter table lwh_products add constraint fk_product_theme foreign key (prod_th_id) references users(th_id);
+alter table lwh_products add constraint fk_product_created_by foreign key (prod_created_by) references lwh_users(usr_id);
+alter table lwh_products add constraint fk_product_modified_by foreign key (prod_modified_by) references lwh_users(usr_id);
+alter table lwh_products add constraint fk_product_theme foreign key (prod_th_id) references lwh_theme(th_id);
