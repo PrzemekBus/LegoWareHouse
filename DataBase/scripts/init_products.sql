f490c7a349d3f74f285d561bb460033d646afe75
pbus <gramp.fot@gmail.com>
2025-03-30 09:44:43 +0200

    created sample data for lego produccts table

---
+++ DataBase/scripts/init_products.sql
@@ -0,0 +1,197 @@
+set define off;
+
+insert into lwh_products (
+    prod_name,
+    prod_eng_name,
+    prod_description,
+    prod_set_number,
+    prod_th_id,
+    prod_release_year,
+    prod_pieces_count,
+    prod_minifigs_count,
+    prod_retail_price,
+    prod_is_active,
+    prod_created_at,
+    prod_created_by
+) values (
+    'Imperial Star Destroyer',
+    'Imperial Star Destroyer',
+    'A massive LEGO set recreating the iconic Imperial Star Destroyer from Star Wars.',
+    '75252',
+    1, 
+    2019,
+    4784,
+    2,
+    699.99,
+    1,
+    systimestamp,
+    1
+);
+
+insert into lwh_products (
+    prod_name,
+    prod_eng_name,
+    prod_description,
+    prod_set_number,
+    prod_th_id,
+    prod_release_year,
+    prod_pieces_count,
+    prod_minifigs_count,
+    prod_retail_price,
+    prod_is_active,
+    prod_created_at,
+    prod_created_by
+) values (
+    'Millennium Falcon',
+    'Millennium Falcon',
+    'The ultimate LEGO Star Wars Millennium Falcon set for collectors.',
+    '75192',
+    1,
+    2017,
+    7541,
+    7,
+    849.99,
+    1,
+    systimestamp,
+    1
+);
+
+insert into lwh_products (
+    prod_name,
+    prod_eng_name,
+    prod_description,
+    prod_set_number,
+    prod_th_id,
+    prod_release_year,
+    prod_pieces_count,
+    prod_minifigs_count,
+    prod_retail_price,
+    prod_is_active,
+    prod_created_at,
+    prod_created_by
+) values (
+    'X-Wing Starfighter',
+    'X-Wing Starfighter',
+    'A detailed LEGO model of the iconic X-Wing Starfighter from Star Wars.',
+    '75301',
+    1, 
+    2021,
+    474,
+    2,
+    49.99,
+    1,
+    systimestamp,
+    1
+);
+
+insert into lwh_products (
+    prod_name,
+    prod_eng_name,
+    prod_description,
+    prod_set_number,
+    prod_th_id,
+    prod_release_year,
+    prod_pieces_count,
+    prod_minifigs_count,
+    prod_retail_price,
+    prod_is_active,
+    prod_created_at,
+    prod_created_by
+) values (
+    'Bugatti Chiron',
+    'Bugatti Chiron',
+    'A highly detailed LEGO Technic model of the Bugatti Chiron supercar.',
+    '42083',
+    2, 
+    2018,
+    3599,
+    0,
+    349.99,
+    1,
+    systimestamp,
+    1
+);
+
+insert into lwh_products (
+    prod_name,
+    prod_eng_name,
+    prod_description,
+    prod_set_number,
+    prod_th_id,
+    prod_release_year,
+    prod_pieces_count,
+    prod_minifigs_count,
+    prod_retail_price,
+    prod_is_active,
+    prod_created_at,
+    prod_created_by
+) values (
+    'Lamborghini Sián FKP 37',
+    'Lamborghini Sián FKP 37',
+    'A LEGO Technic model of the Lamborghini Sián FKP 37 with realistic details.',
+    '42115',
+    2,
+    2020,
+    3696,
+    0,
+    449.99,
+    1,
+    systimestamp,
+    1
+);
+
+insert into lwh_products (
+    prod_name,
+    prod_eng_name,
+    prod_description,
+    prod_set_number,
+    prod_th_id,
+    prod_release_year,
+    prod_pieces_count,
+    prod_minifigs_count,
+    prod_retail_price,
+    prod_is_active,
+    prod_created_at,
+    prod_created_by
+) values (
+    'Police Station',
+    'Police Station',
+    'A LEGO City Police Station set with vehicles and minifigures.',
+    '60246',
+    3, 
+    2020,
+    743,
+    6,
+    99.99,
+    1,
+    systimestamp,
+    1
+);
+
+insert into lwh_products (
+    prod_name,
+    prod_eng_name,
+    prod_description,
+    prod_set_number,
+    prod_th_id,
+    prod_release_year,
+    prod_pieces_count,
+    prod_minifigs_count,
+    prod_retail_price,
+    prod_is_active,
+    prod_created_at,
+    prod_created_by
+) values (
+    'Fire Station',
+    'Fire Station',
+    'A LEGO City Fire Station set with vehicles and minifigures.',
+    '60215',
+    3,
+    2019,
+    509,
+    4,
+    69.99,
+    1,
+    systimestamp,
+    1
+);
