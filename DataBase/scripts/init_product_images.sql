6a5ac73efcaa7f06b1ec044bde6d6eff6a6bb34f
pbus <gramp.fot@gmail.com>
2025-03-30 21:29:57 +0200

    created sample data for products image

---
+++ DataBase/scripts/init_product_images.sql
@@ -0,0 +1,98 @@
+set define off
+insert into lwh_product_images (
+    prod_id,
+    img_url,
+    img_is_primary,
+    img_created_at,
+    img_created_by
+) values (
+    16,
+    'https://www.lego.com/cdn/cs/set/assets/blt934044fa508776e2/75252.jpg?format=webply&fit=bounds&quality=75&width=1200&height=1200&dpr=1',
+    1,
+    systimestamp,
+    1
+);
+
+insert into lwh_product_images (
+    prod_id,
+    img_url,
+    img_is_primary,
+    img_created_at,
+    img_created_by
+) values (
+    17,
+    'https://www.lego.com/cdn/cs/set/assets/bltecd03cc9fb82fd56/75192_alt2.jpg?format=webply&fit=bounds&quality=75&width=1200&height=1200&dpr=1',
+    1,
+    systimestamp,
+    1
+);
+
+insert into lwh_product_images (
+    prod_id,
+    img_url,
+    img_is_primary,
+    img_created_at,
+    img_created_by
+) values (
+    18,
+    'https://www.lego.com/cdn/cs/set/assets/blte1238e85dc34c0a6/75301.jpg?format=webply&fit=bounds&quality=75&width=1200&height=1200&dpr=1',
+    1,
+    systimestamp,
+    1
+);
+
+insert into lwh_product_images (
+    prod_id,
+    img_url,
+    img_is_primary,
+    img_created_at,
+    img_created_by
+) values (
+    19,
+    'https://www.lego.com/cdn/cs/set/assets/bltd13aa93d1b640045/42083.jpg?format=webply&fit=bounds&quality=75&width=1200&height=1200&dpr=1',
+    1,
+    systimestamp,
+    1
+);
+
+insert into lwh_product_images (
+    prod_id,
+    img_url,
+    img_is_primary,
+    img_created_at,
+    img_created_by
+) values (
+    20,
+    'https://www.lego.com/cdn/cs/set/assets/bltf3cb6c55617a056f/42115_alt2.jpg?format=webply&fit=bounds&quality=75&width=1200&height=1200&dpr=1',
+    1,
+    systimestamp,
+    1
+);
+
+insert into lwh_product_images (
+    prod_id,
+    img_url,
+    img_is_primary,
+    img_created_at,
+    img_created_by
+) values (
+    21,
+    'https://www.lego.com/cdn/cs/set/assets/blt8afde7f82f435794/60246_alt2.jpg?format=webply&fit=bounds&quality=75&width=1200&height=1200&dpr=1',
+    1,
+    systimestamp,
+    1
+);
+
+insert into lwh_product_images (
+    prod_id,
+    img_url,
+    img_is_primary,
+    img_created_at,
+    img_created_by
+) values (
+    22,
+    'https://www.lego.com/cdn/cs/set/assets/blteaff3af7ca24d6d2/60215_alt2.png?format=webply&fit=bounds&quality=75&width=1200&height=1200&dpr=1',
+    1,
+    systimestamp,
+    1
+);
