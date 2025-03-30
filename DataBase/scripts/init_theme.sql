c91f740db8de131bcf1de91df5c338cefea58897
pbus <gramp.fot@gmail.com>
2025-03-30 09:39:52 +0200

    created sample data for lego themes table

---
+++ DataBase/scripts/init_theme.sql
@@ -0,0 +1,171 @@
+set define off;
+
+insert into lwh_theme (
+    th_name,
+    th_description,
+    th_logo_html,
+    th_is_active,
+    th_created_at,
+    th_created_by
+) values (
+    'Star Wars',
+    'A theme based on the Star Wars universe.',
+    'https://www.lego.com/cdn/cs/set/assets/blteecd4a67e5ed390b/LOGO_sw_standard_2hy17_original_cmyk_1.png?format=png&height=60&dpr=1',
+    1,
+    systimestamp,
+    1
+);
+
+insert into lwh_theme (
+    th_name,
+    th_description,
+    th_logo_html,
+    th_is_active,
+    th_created_at,
+    th_created_by
+) values (
+    'Technic',
+    'A theme focused on advanced building techniques and mechanical functions.',
+    'https://www.lego.com/cdn/cs/set/assets/bltca9ab01c598efe66/Technic-Logo.png?format=png&height=60&dpr=1',
+    1,
+    systimestamp,
+    1
+);
+
+insert into lwh_theme (
+    th_name,
+    th_description,
+    th_logo_html,
+    th_is_active,
+    th_created_at,
+    th_created_by
+) values (
+    'City',
+    'A theme representing everyday city life.',
+    'https://www.lego.com/cdn/cs/set/assets/blt159dfa2ca7af7ef3/city-logo.png?format=png&height=60&dpr=1',
+    1,
+    systimestamp,
+    1
+);
+
+insert into lwh_theme (
+    th_name,
+    th_description,
+    th_logo_html,
+    th_is_active,
+    th_created_at,
+    th_created_by
+) values (
+    'Harry Potter',
+    'A theme based on the magical world of Harry Potter.',
+    'https://www.lego.com/cdn/cs/set/assets/blt244f27d25288f0eb/HP-logo-WW.png?format=png&height=60&dpr=1',
+    1,
+    systimestamp,
+    1
+);
+
+insert into lwh_theme (
+    th_name,
+    th_description,
+    th_logo_html,
+    th_is_active,
+    th_created_at,
+    th_created_by
+) values (
+    'Ninjago',
+    'A theme featuring ninja warriors and their adventures.',
+    'https://www.lego.com/cdn/cs/set/assets/blt73edf1c375fefe0d/ninjago_white_desktop_center_logo.png?format=png&height=60&dpr=1',
+    1,
+    systimestamp,
+    1
+);
+
+insert into lwh_theme (
+    th_name,
+    th_description,
+    th_logo_html,
+    th_is_active,
+    th_created_at,
+    th_created_by
+) values (
+    'Friends',
+    'A theme designed for creative play with a focus on friendship.',
+    'https://www.lego.com/cdn/cs/set/assets/bltbec084123b35e29b/friends_logo_pos_2025.png?format=png&height=60&dpr=1',
+    1,
+    systimestamp,
+    1
+);
+
+insert into lwh_theme (
+    th_name,
+    th_description,
+    th_logo_html,
+    th_is_active,
+    th_created_at,
+    th_created_by
+) values (
+    'Creator Expert',
+    'A theme for advanced builders with detailed and challenging models.',
+    'https://www.lego.com/cdn/cs/set/assets/blt58887f6b988deefa/creator-expert-logo.png?format=png&height=60&dpr=1',
+    1,
+    systimestamp,
+    1
+);
+
+insert into lwh_theme (
+    th_name,
+    th_description,
+    th_logo_html,
+    th_is_active,
+    th_created_at,
+    th_created_by
+) values (
+    'Architecture',
+    'A theme celebrating iconic buildings and architectural designs.',
+    'https://www.lego.com/cdn/cs/set/assets/blt942a7ef120aac7fe/architecture-logo.png?format=png&height=60&dpr=1',
+    1,
+    systimestamp,
+    1
+);
+
+insert into lwh_theme (
+    th_name,
+    th_description,
+    th_logo_html,
+    th_is_active,
+    th_created_at,
+    th_created_by
+) values (
+    'Speed Champions',
+    'A theme featuring famous cars and racing vehicles.',
+    'https://www.lego.com/cdn/cs/set/assets/blt091bf7331e791c4a/Speed_Champions-Logo.png?format=png&height=60&dpr=1',
+    1,
+    systimestamp,
+    1
+);
+
+insert into lwh_theme (
+    th_name,
+    th_description,
+    th_logo_html,
+    th_is_active,
+    th_created_at,
+    th_created_by
+) values (
+    'Minecraft',
+    'A theme based on the popular video game Minecraft.',
+    'https://www.lego.com/cdn/cs/set/assets/blt0350792359218456/Minecraft_logo_top.png?format=png&height=60&dpr=1',
+    1,
+    systimestamp,
+    1
+);
+
+
+
+
+
+
+
+
+    
+
